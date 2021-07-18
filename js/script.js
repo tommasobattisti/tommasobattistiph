80d07407b3191fd29b61ddde4619c079bc915b62
tommasobattisti <82956419+tommasobattisti@users.noreply.github.com>
2021-07-18 15:50:54 +0200

    upload 18.07 15:48
    Hide navbar on scroll

---
+++ js/script.js
@@ -0,0 +1,10 @@
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-50px";
+  }
+  prevScrollpos = currentScrollPos;
+}

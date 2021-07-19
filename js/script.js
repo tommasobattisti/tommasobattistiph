20493d9b32e9405f121dff0b4fd3bcbdf60cc73d
tommasobattisti <82956419+tommasobattisti@users.noreply.github.com>
2021-07-19 16:45:29 +0200

    19.07 16:43
    Changes in meta description and in navbar animation

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ window.onscroll = function() {
   if (prevScrollpos > currentScrollPos) {
     document.getElementById("navbar").style.top = "0";
   } else {
-    document.getElementById("navbar").style.top = "-50px";
+    document.getElementById("navbar").style.top = "-75px";
   }
   prevScrollpos = currentScrollPos;
 }
